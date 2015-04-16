e50521bb8bf760214cf65be9abed42042a3d3f4e
unknown <dbhagyar@DBHAGYAR-WS01.cisco.com>
2015-04-17 00:09:46 +0530

    JENTEST-1: Adding junit jar file

--- javafiles/HelloWorld.java
+++ javafiles/HelloWorld.java
@@ -1,9 +1,5 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
 public class HelloWorld {
 
-    @Test
     public static void main(String[] args) {
         System.out.println("Hello, World");
 		System.out.println("Hi, World");
